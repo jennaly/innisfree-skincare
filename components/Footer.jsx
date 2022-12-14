6c7fc050abade09232746610ac848f49e1162547
jennaly <106183040+jennaly@users.noreply.github.com>
2022-12-14 12:06:02 -0800

    add footer

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,9 +1,20 @@
-import React from 'react'
+import React from 'react';
+import { AiFillInstagram, AiOutlineTwitter, AiOutlineFacebook } from 'react-icons/ai';
 
 const Footer = () => {
     return (
         <div>
-            Footer
+            <footer className="footer items-center p-4 bg-neutral text-neutral-content">
+                <div className="items-center grid-flow-col">
+                    
+                    <p>@Innisfree 2022</p>
+                </div> 
+                <div className="grid-flow-col gap-4 md:place-self-center md:justify-self-end">
+                    <AiOutlineFacebook />
+                    <AiFillInstagram />
+                    <AiOutlineTwitter />
+                </div>
+            </footer>
         </div>
     )
 }
