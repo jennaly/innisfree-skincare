4aaa515bd72b174e97692ccf8b6ebdc609ed8754
jennaly <106183040+jennaly@users.noreply.github.com>
2022-12-14 15:35:24 -0800

    structure navbar

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,9 +1,26 @@
-import React from 'react'
+import React from 'react';
+import Link from 'next/link';
+import { AiOutlineShopping } from 'react-icons/ai';
+
+import { Cart } from './';
+
 
 const Navbar = () => {
     return (
-        <div>
-            
+        <div className="flex justify-between">
+             <p className="logo">
+                <Link href="/">JL Headphones</Link>
+            </p>
+
+            <button type="button"
+            className="cart-icon" 
+            onClick="">
+                <AiOutlineShopping />
+                <span>0</span>
+            </button>
+
+          
+
         </div>
     )
 }
