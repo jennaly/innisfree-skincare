a8038dac27651441fa331f78821b6273c231b09d
jennaly <106183040+jennaly@users.noreply.github.com>
2022-12-15 13:48:13 -0800

    incorporate state to navbar, structure Cart side drawer

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -3,9 +3,11 @@ import Link from 'next/link';
 import { AiOutlineShopping } from 'react-icons/ai';
 
 import { Cart } from './';
+import { useStateContext } from '../context/StateContext'
 
 
 const Navbar = () => {
+    const { showCart, setShowCart, totalQuantities } = useStateContext();
     return (
         <div className="flex justify-between">
              <p className="logo">
@@ -14,12 +16,12 @@ const Navbar = () => {
 
             <button type="button"
             className="cart-icon" 
-            onClick="">
+            onClick={() => setShowCart(true)}>
                 <AiOutlineShopping />
-                <span>0</span>
+                <span className="cart-item-qty">{totalQuantities}</span>
             </button>
 
-          
+            {showCart && <Cart />}
 
         </div>
     )
