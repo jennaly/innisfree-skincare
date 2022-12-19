6af769d760b3c96c9c915e6818a045bbb3970da0
jennaly <106183040+jennaly@users.noreply.github.com>
2022-12-19 14:08:40 -0800

    working index page

--- pages/index.js
+++ pages/index.js
@@ -6,23 +6,32 @@ import Link from 'next/link';
 const Home = ({ featuredProductsData, heroBannerData, footerBannerData } ) => {
 
   return (
-
-    <div>
       <>  
         <HeroBanner heroBanner={heroBannerData.length && heroBannerData[0]} />
 
-        <div className="hero min-h-screen bg-base-100 p-8" id="about">
+      <div className="min-h-screen flex flex-col justify-center px-8">
+        <div className="hero bg-base-100 py-8 my-10" id="about">
           <div className="flex flex-col lg:flex-row gap-10">
-            <img src="../jeju_island.jpeg" className=" w-full lg:max-w-xl rounded-lg shadow-2xl" />
-            <div className="max-w-lg lg:px-10 lg:my-auto">
+            <img src="../jeju_island.jpeg" className="lg:max-w-xl rounded-lg shadow-2xl" />
+            <div className="max-w-lg lg:w-2/3 lg:my-auto">
               <h3 className="text-3xl lg:text-5xl font-bold">Where Beauty Meets Sustainability</h3>
               <p className="py-6">Our skin care products are formulated with an average of 80% of naturally-derived ingredients responsively-sourced from the pure and protected island of Jeju. From its lush forests to its crystal sea, we search every corner to handpick the finest ingredients nature has to offer. </p>
-
             </div>
           </div>
         </div>
 
-        <div className="hero min-h-screen flex flex-col justify-center bg-base-100 gap-8" id="featured">
+        <div className="hero bg-base-100 py-8 my-10" id="about">
+          <div className="flex flex-col lg:flex-row gap-10">
+            <div className="max-w-lg lg:my-auto">
+              <h3 className="text-3xl lg:w-2/3 lg:text-5xl font-bold">Skincare Powered by Nature's Finest</h3>
+              <p className="py-6">Innisfree’s proprietary extraction methods preserve the purity and potency of these wholesome ingredients from plant to bottle, offering advanced formulas that safely address all skin concerns without the use of harmful chemicals and preservatives.</p>
+            </div>
+            <img src="../skincare_model.jpg" className="lg:max-w-xl rounded-lg shadow-2xl" />
+          </div>
+        </div>
+      </div>
+
+        <div className="hero flex flex-col justify-center bg-base-100 gap-8 my-10" id="featured">
             <div className="flex flex-col items-center gap-2 px-8">
               <span className="mt-6 lg:text-xl">Clean Beauty Made Easy</span>
               <h2 className="text-5xl font-bold text-center">The Latest Essentials</h2>
@@ -45,7 +54,6 @@ const Home = ({ featuredProductsData, heroBannerData, footerBannerData } ) => {
         <FooterBanner footerBanner={footerBannerData.length && footerBannerData[0]} />
 
       </>
-    </div>
   )
 }
 
