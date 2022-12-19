ab050fcaaeca1e864564e448c46bc2675205c680
jennaly <106183040+jennaly@users.noreply.github.com>
2022-12-19 14:30:21 -0800

    add links to footer'

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,18 +1,19 @@
 import React from 'react';
-import { AiFillInstagram, AiOutlineTwitter, AiOutlineFacebook } from 'react-icons/ai';
+import { AiFillInstagram, AiOutlineTwitter, AiFillFacebook } from 'react-icons/ai';
+import Link from 'next/link'
 
 const Footer = () => {
     return (
         <div>
-            <footer className="footer items-center p-4 bg-neutral text-neutral-content">
+            <footer className="footer items-center p-4">
                 <div className="items-center grid-flow-col">
                     
                     <p>@Innisfree 2022</p>
                 </div> 
-                <div className="grid-flow-col gap-4 md:place-self-center md:justify-self-end">
-                    <AiOutlineFacebook />
-                    <AiFillInstagram />
-                    <AiOutlineTwitter />
+                <div className="grid-flow-col gap-4 md:place-self-center md:justify-self-end icons">
+                    <Link href="/"><AiFillInstagram /></Link>
+                    <Link href="/"><AiOutlineTwitter /></Link>
+                    <Link href="/"><AiFillFacebook /></Link>
                 </div>
             </footer>
         </div>
