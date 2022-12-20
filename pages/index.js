7b8907f912044e44b9ffc173db800e7b55ee2251
jennaly <106183040+jennaly@users.noreply.github.com>
2022-12-20 14:11:26 -0800

    reposition main copy

--- pages/index.js
+++ pages/index.js
@@ -9,19 +9,19 @@ const Home = ({ featuredProductsData, heroBannerData, footerBannerData } ) => {
       <>  
         <HeroBanner heroBanner={heroBannerData.length && heroBannerData[0]} />
 
-      <div className="min-h-screen flex flex-col justify-center px-8" id="about">
-        <div className="hero bg-base-100 py-8 my-10">
-          <div className="flex flex-col lg:flex-row gap-10">
+      <div className="min-h-screen flex flex-col justify-center px-8 gap-5 lg:gap-10" id="about">
+        <div className="hero bg-base-100 lg:py-8 mt-10">
+          <div className="flex flex-col lg:flex-row lg:gap-10 md:max-w-lg lg:max-w-full">
             <img src="../jeju_island.jpeg" className="lg:max-w-xl rounded-lg shadow-2xl" />
             <div className="max-w-lg lg:w-2/3 lg:my-auto">
-              <h3 className="text-3xl lg:text-5xl font-bold">Where Beauty Meets Sustainability</h3>
+              <h3 className="text-3xl lg:text-5xl font-bold mt-6">Where Beauty Meets Sustainability</h3>
               <p className="py-6">Our skin care products are formulated with an average of 80% of naturally-derived ingredients responsively-sourced from the pure and protected island of Jeju. From its lush forests to its crystal sea, we search every corner to handpick the finest ingredients nature has to offer. </p>
             </div>
           </div>
         </div>
 
-        <div className="hero bg-base-100 py-8 my-10">
-          <div className="flex flex-col lg:flex-row gap-10">
+        <div className="hero bg-base-100 lg:py-8 mb-10">
+          <div className="flex flex-col lg:flex-row lg:gap-10 md:max-w-lg lg:max-w-full">
             <div className="max-w-lg lg:my-auto">
               <h3 className="text-3xl lg:w-2/3 lg:text-5xl font-bold">Skincare Powered by Nature's Finest</h3>
               <p className="py-6">Innisfree’s proprietary extraction methods preserve the purity and potency of these wholesome ingredients from plant to bottle, offering advanced formulas that safely address all skin concerns without the use of harmful chemicals and preservatives.</p>
