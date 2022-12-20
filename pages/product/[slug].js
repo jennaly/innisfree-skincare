48a727a3b1b089467993f986aaeb315be63d2985
jennaly <106183040+jennaly@users.noreply.github.com>
2022-12-20 14:09:37 -0800

    style quantity selector, position hero, style buttons

--- pages/product/[slug].js
+++ pages/product/[slug].js
@@ -5,6 +5,7 @@ import { client, urlFor } from '../../lib/client';
 import { Product } from '../../components';
 import { useStateContext } from '../../context/StateContext';
 import { useRouter } from 'next/router';
+import Link from 'next/link';
 
 const ProductDetails = ({ product, products, productsFromSameLine }) => {
 
@@ -57,13 +58,21 @@ const ProductDetails = ({ product, products, productsFromSameLine }) => {
 
     return (
         <div>
-           <div className="hero min-h-screen bg-base-200">
-                <div className="hero-content flex-col lg:flex-row gap-10">
-                    <div>
-                        <img src={urlFor(image && image[productImageIndex])} className="max-w-xl rounded-lg shadow-2xl" />
-                        <div className="small-images-container flex justify-center gap-6">
+            <div className="min-h-screen bg-base-100 px-5px">
+                <div className="text-sm breadcrumbs flex lg:text-lg lg:mt-3 ">
+                        <ul className="mx-auto">
+                            <li><Link href="/">Home</Link></li>
+                            <li><Link href="/">Products</Link></li>
+                            <li><Link href="/">{name}</Link></li>
+                        </ul>
+                </div>
+                
+                <div className="flex flex-col lg:flex-row gap-10 mt-3 lg:mt-10 w-11/12 lg:w-10/12 mx-auto justify-center">
+                    <div className="md:max-w-lg md:mx-auto lg:mx-0 lg:w-1/3">
+                        <img src={urlFor(image && image[productImageIndex])} className="shadow-2xl w-full product-image" />
+                        <div className="small-images-container flex justify-between gap-6 w-full">
                             {image?.map((item, i) => (
-                                <div className="w-36 shadow-xl mt-4" key={i}>
+                                <div className="shadow-xl mt-4" key={i}>
                                     <img
                                         key={i} 
                                         src={urlFor(item)}
@@ -75,37 +84,45 @@ const ProductDetails = ({ product, products, productsFromSameLine }) => {
                         </div>
                     </div>
                     
-                    <div className="max-w-lg">
-                        <h2 className="text-5xl font-bold">{name}</h2>
-                        <p className="py-6">{details}</p>
-                        <p className="py-6">${price.toFixed(2)}</p>
-                        <div className="quantity">
-                            <h3>Quantity:</h3>
-                            <p className="quantity-desc flex">
-                                <span className="minus outline p-2" onClick={decQty}><AiOutlineMinus /></span>
-                                <span className="num outline p-2 px-4">{qty}</span>
-                                <span className="plus outline p-2" onClick={incQty}><AiOutlinePlus /></span>
+                    <div className="flex flex-col max-w-lg mx-auto lg:mx-0 my-auto gap-4">
+                        <h1 className="text-3xl font-bold">{name}</h1>
+                        <h2 className="">${price.toFixed(2)}</h2>
+                        <p className="">{details}</p>
+                        <div className="quantity flex flex-row w-full items-center justify-between gap-4">
+                            <h3>Quantity</h3>
+                            <p className="flex product-quantity-selector rounded-md items-center w-full p-2 justify-between bg-base-200">
+                                <button className="w-1/6 " onClick={decQty}><AiOutlineMinus className="product-quantity"/></button>
+                                <span className="num inline-block p-2 px-4 w-4/6 text-center ">{qty}</span>
+                                <button className="w-1/6" onClick={incQty}><AiOutlinePlus className="product-quantity"/></button>
                             </p>
-
-                            <div className="buttons flex gap-4">
-                                <button type="button" className="add-to-cart btn btn-primary" onClick={() => onAdd(product, qty)}>Add to Cart</button>
-                                <button type="button" className="buy-now btn btn-secondary" onClick={ handleBuyNow }>Buy Now</button>
-                            </div>
+                        </div>
+                        <div className="buttons flex justify-between gap-4">
+                            <button type="button" className="add-to-cart btn bg-green-700 hover:bg-green-600 border-none" onClick={() => onAdd(product, qty)}>Add to Cart</button>
+                            <button type="button" className="buy-now btn btn-secondary bg-base-200 hover:bg-base-100 border border-green-700 hover:border-green-600 text-black" onClick={ handleBuyNow }>Buy Now</button>
                         </div>
                     </div>
                 </div>
 
             </div>
 
-            <div>
-                <h2>Explore the rest of the {product.productLine} Collection</h2>
-
-                <div className="flex justify-center gap-6">
-                    {productsFromSameLine?.map((product => <Product key={product._id} product={product}/>))}
+            <div className="hero flex flex-col justify-center bg-base-100 gap-8 my-10">
+                <div className="flex flex-col items-center gap-2 px-8">
+                    <h3 className="text-5xl font-bold text-center">Explore the {product.productLine} Collection</h3>
                 </div>
 
+                <div className="flex flex-col lg:flex-row">
+                {productsFromSameLine.map((product, index) => {
+                    return (
+                    <div className="card w-full" key={index}>
+                        <div className="card-body">
+                            <Product key={product._id} product={product} dimensions={500}/>
+                        </div>
+                    </div>
+                    )})}
+                </div>
             </div>
-        </div>
+            
+        </div> 
     )
 }
 
