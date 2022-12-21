5c39b0821b49fb7f30f1cacc6a349584b4be4de4
jennaly <106183040+jennaly@users.noreply.github.com>
2022-12-20 20:23:00 -0800

    re-position store logo and navbar componentsg

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -9,39 +9,31 @@ const Navbar = () => {
     const { showCart, setShowCart, totalQuantities } = useStateContext();
     return (
 
-        <div className="navbar-container navbar bg-base-100">
+        <div className="navbar-container navbar bg-base-100 md:w-10/12 xl:w-9/12 mx-auto">
 
             <div className="navbar-start">
 
                 <div className="dropdown">
-                <label tabIndex={0} className="btn btn-ghost lg:hidden">
-                    <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
-                </label>
-                <ul tabIndex={0} className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
-                    <li><Link href="/">Home</Link></li>
-                    <li tabIndex={0}>
-                        <a className="justify-between">
-                            Shop
-                            <svg className="fill-current" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M8.59,16.58L13.17,12L8.59,7.41L10,6L16,12L10,18L8.59,16.58Z"/></svg>
-                        </a>
-                        <ul className="p-2 bg-base-100">
-                            <li><Link href="#featured">New</Link></li>
-                            <li><Link href="/">All Products</Link></li>
-                        </ul>
-                    </li>
-                    <li><Link href="#about">About Us</Link></li>
-                </ul>
+                    <label tabIndex={0} className="btn btn-ghost lg:hidden">
+                        <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
+                    </label>
+                    <ul tabIndex={0} className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
+                        <li><Link href="/">Home</Link></li>
+                        <li tabIndex={0}>
+                            <a className="justify-between">
+                                Shop
+                                <svg className="fill-current" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M8.59,16.58L13.17,12L8.59,7.41L10,6L16,12L10,18L8.59,16.58Z"/></svg>
+                            </a>
+                            <ul className="p-2 bg-base-100">
+                                <li><Link href="#featured">New</Link></li>
+                                <li><Link href="/">All Products</Link></li>
+                            </ul>
+                        </li>
+                        <li><Link href="#about">About Us</Link></li>
+                    </ul>
                 </div>
 
-                <p className="btn btn-ghost normal-case text-xl">
-                    <Link href="/">Innisfree</Link>
-                </p>
-
-            </div>
-
-            <div className="navbar-center hidden lg:flex">
-              
-                <ul className="menu menu-horizontal px-1">
+                <ul className="menu menu-horizontal px-1 hidden lg:flex">
                     <li><Link href="/">Home</Link></li>
                     <li tabIndex={0}>
                         <Link href="/">
@@ -58,6 +50,18 @@ const Navbar = () => {
                     <li><Link href="#about">About Us</Link></li>
                 </ul>
 
+                <p className="btn btn-ghost normal-case text-xl lg:hidden">
+                    <Link href="/">Innisfree</Link>
+                </p>
+
+            </div>
+
+            <div className="navbar-center ">
+
+                <p className="btn btn-ghost normal-case text-xl hidden lg:flex">       
+                    <Link href="/">Innisfree</Link>
+                </p>
+
             </div>
 
             <div className="navbar-end navbar-cart">
@@ -67,15 +71,13 @@ const Navbar = () => {
                     className="cart-icon" 
                     onClick={() => setShowCart(true)}>
                         <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z" /></svg>
-                        <span className="badge badge-sm indicator-item">{totalQuantities}</span>
+                        <span className="badge badge-sm indicator-item bg-green-700">{totalQuantities}</span>
                     </button>
                 </div>
 
                 {showCart && <Cart />}
 
             </div>
-
-            
 
         </div>
     )
