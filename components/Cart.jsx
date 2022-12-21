3910d733c687adbf332c664a4dc4f133d65df664
jennaly <106183040+jennaly@users.noreply.github.com>
2022-12-20 22:03:51 -0800

    style cart components

--- components/Cart.jsx
+++ components/Cart.jsx
@@ -1,6 +1,7 @@
 import React, {useRef} from 'react';
 import Link from 'next/link';
-import { AiOutlineMinus, AiOutlinePlus, AiOutlineLeft, AiOutlineShopping } from 'react-icons/ai';
+import { AiOutlineMinus, AiOutlinePlus, AiOutlineShopping } from 'react-icons/ai';
+import { TfiClose } from 'react-icons/tfi'
 import { TiDelete, TiDeleteOutline } from 'react-icons/ti';
 import toast from 'react-hot-toast';
 
@@ -36,74 +37,73 @@ const Cart = () => {
 
     return (
         <div className="cart-wrapper" ref={cartRef}>
-            <div className="cart-container">
-                <button 
-                    type="button"
-                    className="cart-heading"
-                    onClick={() => setShowCart(false)}>
-                        <AiOutlineLeft />
-                        <span>Your Cart</span>
-                        <span>({totalQuantities}) items</span>
-                    </button>
-
-                {cartItems.length < 1 && (
-                    <div className="empty-cart">
-                        <AiOutlineShopping size={150} className="mx-auto" />
-                        <h3>Your shopping bag is empty</h3>
-                        <Link href="/">
-                            <button 
-                                type="button"
-                                onClick={() => setShowCart(false)}
-                                className="btn"
-                            >
-                                Continue Shopping
-                            </button>
-                        </Link>
-                    </div>
-                )}  
-
+            <div className="cart-container w-full md:w-[450px] xl:w-[500px] px-5 lg:px-10 py-10 lg:py-5 flex flex-col justify-between">
                 <div>
-                    {cartItems.length >= 1 && cartItems.map((item, index) => (
-                        <div key={item._id}>
-                            <img src={urlFor(item?.image[0])} />
-                            <div>
-                                <div className="flex top">
-                                    <h5>{item.name}</h5>
-                                    <h4>${item.price.toFixed(2)}</h4>
-                                </div>
-                                <div className="flex bottom justify-between">
-                                    <div>
-                                        <p className="quantity-desc flex">
-                                            <span className="minus outline p-2" onClick={() => toggleCartItemQuantity(item._id, 'dec')}><AiOutlineMinus /></span>
-                                            <span className="num outline p-2 px-4">{item.quantity}</span>
-                                            <span className="plus outline p-2" onClick={() => toggleCartItemQuantity(item._id, 'inc')}><AiOutlinePlus /></span>
-                                        </p>
-                                    </div>
+                    <button 
+                        type="button"
+                        className="flex w-full justify-between items-center mb-5"
+                        onClick={() => setShowCart(false)}>
+                            <h1 className="inline-block text-xl">Shopping Cart</h1>
+                            <TfiClose className="inline-block" />
+                        </button>
 
-                                    <button 
-                                        type="button"
-                                        className=""
-                                        onClick={() => onRemove(item)}
-                                    >
-                                        <TiDeleteOutline />
-                                    </button>
+                    {cartItems.length < 1 && (
+                        <div className="empty-cart">
+                            <AiOutlineShopping size={150} className="mx-auto" />
+                            <h3>Your shopping bag is empty</h3>
+                            <Link href="/">
+                                <button 
+                                    type="button"
+                                    onClick={() => setShowCart(false)}
+                                    className="btn"
+                                >
+                                    Continue Shopping
+                                </button>
+                            </Link>
+                        </div>
+                    )}  
+
+                    <div className="flex flex-col gap-5">
+                        {cartItems.length >= 1 && cartItems.map((item, index) => (
+                            <div key={item._id} className="flex w-full gap-5 mt-3">
+                                <img src={urlFor(item?.image[0])} className="w-1/2 md:w-1/3"/>
+                                <div className="flex flex-col w-2/3 my-auto gap-2">
+                                    <div className="flex flex-col">
+                                        <h2 className="w-full text-lg">{item.name}</h2>
+                                        <span className="inline-block w-full">${item.price.toFixed(2)}</span>
+                                    </div>
+                                    <div className="flex flex-col gap-2 w-full md:w-1/2">
+                                        <p className="flex product-quantity-selector rounded-md items-center w-full px-2 py-1 justify-between bg-base-200">
+                                            <button className="w-1/6 " onClick={() => toggleCartItemQuantity(item._id, 'dec')}><AiOutlineMinus className="product-quantity"/></button>
+                                            <span className="num inline-block p-2 px-4 w-4/6 text-center ">{item.quantity}</span>
+                                            <button className="w-1/6" onClick={() => toggleCartItemQuantity(item._id, 'inc')}><AiOutlinePlus className="product-quantity"/></button>
+                                        </p>
+
+                                        <button 
+                                            type="button"
+                                            className="text-sm text-left"
+                                            onClick={() => onRemove(item)}
+                                        >
+                                            Remove
+                                        </button>
+                                    </div>
                                 </div>
                             </div>
-                        </div>
-                    ))}
+                        ))}
+                    </div>
                 </div>
 
                 {cartItems.length >= 1 && (
-                    <div className="cart-bottom">
-                        <div className="total">
-                            <h3>Subtotal:</h3>
-                            <h3>${totalPrice.toFixed(2)}</h3>
+                    <div className="flex flex-col gap-4">
+                        <div className="flex justify-between text-lg">
+                            <h3 >Subtotal</h3>
+                            <span>${totalPrice.toFixed(2)}</span>
                         </div>
                    
 
                         <div className="btn-container">
-                            <button type="button" className="btn" onClick={ handleCheckout }>
-                                Pay with Stripe
+                            <button type="button" className="btn bg-green-700 hover:bg-green-600 border-none w-full" onClick={ handleCheckout }>
+                                Checkout
 
                             </button>
                         </div>
